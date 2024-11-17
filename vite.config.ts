254ce01dcf1764c2e53115afb56162481c244738
velolib <vlocitize@gmail.com>
2024-11-17 08:52:40 +0700

    Set NODE_ENV to production for build and adjust base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/sprinkler/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
